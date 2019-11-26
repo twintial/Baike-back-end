8cf582d2d27954612333f631fc1d40904e202af4
Aun3ma <1913403015@qq.com>
2019-11-26 13:46:33 +0800

    rua

--- src/main/java/com/example/baike/mapper/InteractiveVideoMapper.java
+++ src/main/java/com/example/baike/mapper/InteractiveVideoMapper.java
@@ -6,13 +6,16 @@ import com.example.baike.model.BKVideoUploadViewModel;
 import com.example.baike.state.VideoState;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface InteractiveVideoMapper {
     Long insertInteractiveVideo(BKVideoUploadViewModel uploadViewModel);
     Long insertVideo(List<BKVideo> videos);
     Long deleteInteractiveVideoByID(@Param("vID") Integer vID);
     List<BKInteractiveVideo> selectInterVideosByUserIf(@Param("uID") Integer uID, @Param("state")VideoState state);
+    List<BKVideo> selectVideoByInterVID(@Param("ivID") Integer ivID);
 }
